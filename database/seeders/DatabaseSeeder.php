e900d410f230fa3c358c06a0327b1418e552ec13
Havva Zeynep Akdemir <83979496+Havvazeynep@users.noreply.github.com>
2022-11-05 22:14:04 +0300

    Manage Listings sayfası yapıldı

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Listing;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(5)->create();
+        // \App\Models\User::factory(5)->create();
+        $user = User::factory()->create([
+            'name' => 'Havva Zeynep',
+            'email' => 'havva@gmail.com'
+        ]);
 
-        Listing::factory(6)->create();
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
 
          // Listing::create([
          //    'title' => 'Laravel Senior Developer', 
